5722916c2688917a5030feb2dbf52462e086a12a
Tyrone Thompson <tyrone.thompson@gmail.com>
2018-06-14 10:40:04 -0400

    James change

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import SayMyName from '../components/saymyname'
 const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
-    <p>Testing this out!!.</p>
+    <p>Look at this James.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
     <SayMyName path='/page-2'/>
