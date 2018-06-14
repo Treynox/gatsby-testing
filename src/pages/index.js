4eb59f4c397a0ec6c064aabc1ab801baccd5c114
Tyrone Thompson <tyrone.thompson@gmail.com>
2018-06-14 10:37:46 -0400

    change initial page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import SayMyName from '../components/saymyname'
 const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>Testing this out!!.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
     <SayMyName path='/page-2'/>
